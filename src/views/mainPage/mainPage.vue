07d80929353b7aa09b1a54eeb6eb5178a1ce077d
Farr1102 <853276905@qq.com>
2021-02-19 14:31:20 +0800

    随便写了一点点

--- src/views/mainPage/mainPage.vue
+++ src/views/mainPage/mainPage.vue
@@ -4,7 +4,29 @@
       <router-link to="/" slot="left">
         <mt-button icon="back"></mt-button>
       </router-link>
-      <mt-button icon="search" slot="right"></mt-button>
+      <mt-button icon="search" slot="right" @click="searchMusic">
+      </mt-button>
     </mt-header>
+    <search-res></search-res>
+       
   </div>
 </template>
+<script>
+import searchRes from "./components/searchRes"
+export default {
+  components: {
+    searchRes
+  },
+  data() {
+
+    return{
+
+    }
+  },
+  methods: {
+    searchMusic() {
+
+    }
+  }
+}
+</script>
